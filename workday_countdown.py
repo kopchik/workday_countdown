56038c31508e98770db3cb2f5c58844740f4e833
Alexandre Kandalintsev <exe.sre@gmail.com>
2016-07-18 01:38:27 +0200

    autopep8 FTW

--- workday_countdown.py
+++ workday_countdown.py
@@ -40,80 +40,79 @@ symap = {add: '+', sub: '-', mul: '*', truediv: '/'}
 
 
 def myeval(nums, ops):
-  """ Evaluates raw expression. May raise exception like ZeroDivisionError. """
-  r = nums[0]
-  for i, op in enumerate(ops):
-    r = op(r, nums[i+1])
-  return r
+    """ Evaluates raw expression. May raise exception like ZeroDivisionError. """
+    r = nums[0]
+    for i, op in enumerate(ops):
+        r = op(r, nums[i + 1])
+    return r
 
 
 def _countdown(numbers, target, operators=operators):
-  """ It plays countdown game (seems very popular in  Ireland).
+    """ It plays countdown game (seems very popular in  Ireland).
 
-  Parameters
-  ----------
-  numbers: list of integers in [0, 1024] (not really checked here)
-  target:  value we should get at the end
-  operators: allowed operation to achieve the target value
+    Parameters
+    ----------
+    numbers: list of integers in [0, 1024] (not really checked here)
+    target:  value we should get at the end
+    operators: allowed operation to achieve the target value
 
-  Returns
-  -------
-  Raw expression along with the result.
-  """
+    Returns
+    -------
+    Raw expression along with the result.
+    """
 
-  best_value = numbers[0]
-  best_expr = ([numbers[0]], [])
-  best_error = abs(best_value - target)
+    best_value = numbers[0]
+    best_expr = ([numbers[0]], [])
+    best_error = abs(best_value - target)
 
-  for i in range(1, len(numbers)+1):
-    for nums in permutations(numbers, i):
-      for ops in combinations_with_replacement(operators, len(nums)-1):
-        try:
-          r = myeval(nums, ops)
-        except Exception:
-          continue
-        err = abs(target - r)
-        #print(">", nums, ops, r, err)
-        if err < best_error:
-          if r == target:
-            return (nums, ops), r  # shortcut if we found solution
-          else:
-            best_expr  = nums, ops
-            best_error = err
-            best_value = r
-  return best_expr, best_value
+    for i in range(1, len(numbers) + 1):
+        for nums in permutations(numbers, i):
+            for ops in combinations_with_replacement(operators, len(nums) - 1):
+                try:
+                    r = myeval(nums, ops)
+                except Exception:
+                    continue
+                err = abs(target - r)
+                #print(">", nums, ops, r, err)
+                if err < best_error:
+                    if r == target:
+                        return (nums, ops), r  # shortcut if we found solution
+                    else:
+                        best_expr = nums, ops
+                        best_error = err
+                        best_value = r
+    return best_expr, best_value
 
 
 def myformat(expr, value):
-  """ It does what is says :) """
-  nums, ops = expr
-  result = [nums[0]]
-  for num, op in zip(nums[1:], ops):
-    result.append(symap[op])
-    result.append(num)
-    if op in [add, sub]:
-      result.insert(0, '(')
-      result.append(')')
-  result.append('=')
-  result.append(value)
-  return " ".join(map(str, result))
+    """ It does what is says :) """
+    nums, ops = expr
+    result = [nums[0]]
+    for num, op in zip(nums[1:], ops):
+        result.append(symap[op])
+        result.append(num)
+        if op in [add, sub]:
+            result.insert(0, '(')
+            result.append(')')
+    result.append('=')
+    result.append(value)
+    return " ".join(map(str, result))
 
 
 def countdown(rawinpt):
-  """ Converts input string into python-friendly format: 6 numbers and target value.
-      Then calls _countdown and myformat to produce the actual result.
-      Also does input validation.
-  """
-  assert isinstance(rawinpt, str), "needs string as input"
-  inpt = list(map(int, rawinpt.split()))
-  assert all(n in range(0, 1024) for n in inpt), "input value not in range"
-  assert len(inpt) == 7, "expected 7 numbers, got %s" % len(inpt)
-  *numbers, target = inpt
-  expr, result = _countdown(numbers, target)
-  return myformat(expr, result)
-
+    """ Converts input string into python-friendly format: 6 numbers and target value.
+        Then calls _countdown and myformat to produce the actual result.
+        Also does input validation.
+    """
+    assert isinstance(rawinpt, str), "needs string as input"
+    inpt = list(map(int, rawinpt.split()))
+    assert all(n in range(0, 1024) for n in inpt), "input value not in range"
+    assert len(inpt) == 7, "expected 7 numbers, got %s" % len(inpt)
+    *numbers, target = inpt
+    expr, result = _countdown(numbers, target)
+    return myformat(expr, result)
 
 
 if __name__ == '__main__':
-  inpt = input("Enter 7 numbers: ")
-  print(countdown(inpt))
+    inpt = input("Enter 7 numbers: ")
+    print(countdown(inpt))
--- workday_countdown_test.py
+++ workday_countdown_test.py
@@ -9,40 +9,42 @@ from operator import mul, add
 import pytest
 import cProfile
 
-def test_countdown_input_validation():
-  # not enough arguments
-  with pytest.raises(AssertionError):
-    countdown("1 2 3")
 
-  # negative arguments
-  with pytest.raises(AssertionError):
-    countdown("1 2 3 4 5 6 -7")
+def test_countdown_input_validation():
+    # not enough arguments
+    with pytest.raises(AssertionError):
+        countdown("1 2 3")
+
+    # negative arguments
+    with pytest.raises(AssertionError):
+        countdown("1 2 3 4 5 6 -7")
 
 
 def test_countdown_correctness():
-  # trivial case
-  assert countdown("1 2 3 4 5 6 1") == "1 = 1"
+    # trivial case
+    assert countdown("1 2 3 4 5 6 1") == "1 = 1"
 
-  # provided examples
-  assert countdown("1 2 3 4 5 6 12") == "2 * 6 = 12"
-  assert countdown("5 8 6 25 13 87 390") == "5 * 6 * 13 = 390"
+    # provided examples
+    assert countdown("1 2 3 4 5 6 12") == "2 * 6 = 12"
+    assert countdown("5 8 6 25 13 87 390") == "5 * 6 * 13 = 390"
 
-  # operations with zeros (incl. div by zero)
-  assert countdown("0 0 0 0 0 0 1000") == "0 = 0"
+    # operations with zeros (incl. div by zero)
+    assert countdown("0 0 0 0 0 0 1000") == "0 = 0"
 
 
 def test_countdown_performance():
-  """ Evaluates the performance in the worst case. """
-  profiler = cProfile.Profile()
-  profiler.enable()
-  countdown("0 0 0 0 0 0 1000")
-  profiler.disable()
-  stats = profiler.getstats()
-  tot_time = stats[0].totaltime
-  assert tot_time < 3, "Wow, your computer is really slow. Or is it my code?"
+    """ Evaluates the performance in the worst case. """
+    profiler = cProfile.Profile()
+    profiler.enable()
+    countdown("0 0 0 0 0 0 1000")
+    profiler.disable()
+    stats = profiler.getstats()
+    tot_time = stats[0].totaltime
+    assert tot_time < 3, "Wow, your computer is really slow. Or is it my code?"
 
 
 def test_myformat():
-  # test parentheses
-  assert myformat(expr=([1,2],[add]),value=3) == "( 1 + 2 ) = 3"
-  assert myformat(expr=([1,2,3],[add, mul]),value=9) == "( 1 + 2 ) * 3 = 9"
+    # test parentheses
+    assert myformat(expr=([1, 2], [add]), value=3) == "( 1 + 2 ) = 3"
+    assert myformat(expr=([1, 2, 3], [add, mul]),
+                    value=9) == "( 1 + 2 ) * 3 = 9"
